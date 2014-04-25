b7c88b29b8926c5d7a30611f96b50f9f2bacb3b6
tyage <namatyage@gmail.com>
2014-04-25 15:05:55 +0900

    タイミング調整

--- Controller.v
+++ Controller.v
@@ -7,66 +7,14 @@ module Controller(
 	// GND, VCC
 	reg GND = 0;
 	reg VCC = 1;
-	reg P1Clock;
 
-	// Memory
-	reg [15:0] memoryAddress;
-	reg [15:0] memoryWriteData;
-	reg memoryWriteEnable;
-	wire [15:0] memoryData;
-	Memory (.address(memoryAddress), .data(memoryWriteData), .wren(memoryWriteEnable), .q(memoryData), .clock(clock));
-
-	// InstructionRegister
-	reg [15:0] IRWriteData;
-	wire [15:0] IRData;
-	InstructionRegister (.writeData(IRWriteData), .loadData(IRData), .clock(P1Clock));
-
-	// ProgramCounter
-	wire [15:0] PC;
-	reg [15:0] PCLoad;
-	ProgramCounter (.clk(P1Clock), .counter(PC), .load(PCLoad));
-
-	//	ALU
-   function [3:0] ALUType(input [3:0] format);
-      case (format)
-			2'b11: ALUType = IRData[7:4];
-			2'b00: ALUType = 4'b0;
-			2'b01: ALUType = 4'b0;
-      endcase
-   endfunction
-   function [15:0] ALUDataA(input [3:0] format);
-      case (format)
-			2'b11: ALUDataA = AR;
-			2'b00: ALUDataA = AR;
-			2'b01: ALUDataA = AR;
-      endcase
-   endfunction
-   function [15:0] ALUDataB(input [3:0] format);
-      case (format)
-			2'b11: ALUDataB = BR;
-			2'b00: ALUDataB = BR;
-			2'b01: ALUDataB = BR;
-      endcase
-   endfunction
-	wire [3:0] ALUFlags;
-	wire [15:0] ALUOut;
-	wire S = ALUFlags[0];
-	wire Z = ALUFlags[1];
-	wire C = ALUFlags[2];
-	wire V = ALUFlags[3];
-	ALU (.S_ALU(ALUType(IRData[15:14])), .DATA_A(ALUDataA(IRData[15:14])), .DATA_B(ALUDataB(IRData[15:14])), .FLAG_OUT(ALUFlags), .ALU_OUT(ALUOut));
-
-	// other registers
+	// registers
 	reg [15:0] registerFile [0:7];
 	reg [15:0] BR;
 	reg [15:0] AR;
 	reg [15:0] DR;
 	reg [15:0] MDR;
 	reg [15:0] result;
-
-	// PhaseCounter
-	wire [4:0] phase;
-	PhaseCounter (.clock(clock), .phase(phase));
 
 	// DEBUG
 	integer i;
@@ -77,26 +25,86 @@ module Controller(
 		registerFile[2] <= 16'b0000_0000_0000_0001;
 	end
 
+	// Memory
+	function [15:0] memoryAddress(
+		input [4:0] phase,
+		input [15:0] IRData);
+		case (phase)
+			// Phase1
+			5'b00001: memoryAddress = PC;
+			// Phase4
+			5'b01000: begin
+				// load, store
+				if (IRData[15:14] == 2'b00 || IRData[15:14] == 2'b01) memoryAddress = DR;
+			end
+		endcase
+	endfunction
+	function [15:0] memoryWriteData(
+		input [4:0] phase,
+		input [15:0] IRData, data);
+		// Phase4 and store
+		if (phase == 5'b01000 && IRData[15:14] == 2'b01) memoryWriteData = data;
+	endfunction
+	function [15:0] memoryWriteEnable(
+		input [4:0] phase,
+		input [15:0] IRData);
+		// Phase4 and store
+		if (phase == 5'b01000 && IRData[15:14] == 2'b01) memoryWriteEnable = VCC;
+		else memoryWriteEnable = GND;
+	endfunction
+	wire [15:0] memoryData;
+	Memory (
+		.address(memoryAddress(phase, IRData)),
+		.data(memoryWriteData(phase, IRData, registerFile[AR])),
+		.wren(memoryWriteEnable(phase, IRData)),
+		.q(memoryData),
+		.clock(clock)
+	);
+
+	// InstructionRegister
+	wire [15:0] IRData;
+	InstructionRegister (.writeData(memoryData), .loadData(IRData), .write(phase == 5'b00001), .clock(clock));
+
+	// ProgramCounter
+	wire [15:0] PC;
+	reg [15:0] PCLoad;
+	ProgramCounter (.clk(phase == 5'b00001), .counter(PC), .load(PCLoad));
+
+	//	ALU
+	function [3:0] ALUType(input [3:0] format);
+		case (format)
+			2'b11: ALUType = IRData[7:4];
+			2'b00: ALUType = 4'b0;
+			2'b01: ALUType = 4'b0;
+		endcase
+	endfunction
+	function [15:0] ALUDataA(input [3:0] format);
+		case (format)
+			2'b11: ALUDataA = AR;
+			2'b00: ALUDataA = AR;
+			2'b01: ALUDataA = AR;
+		endcase
+	endfunction
+	function [15:0] ALUDataB(input [3:0] format);
+		case (format)
+			2'b11: ALUDataB = BR;
+			2'b00: ALUDataB = BR;
+			2'b01: ALUDataB = BR;
+		endcase
+	endfunction
+	wire [3:0] ALUFlags;
+	wire [15:0] ALUOut;
+	wire S = ALUFlags[0];
+	wire Z = ALUFlags[1];
+	wire C = ALUFlags[2];
+	wire V = ALUFlags[3];
+	ALU (.S_ALU(ALUType(IRData[15:14])), .DATA_A(ALUDataA(IRData[15:14])), .DATA_B(ALUDataB(IRData[15:14])), .FLAG_OUT(ALUFlags), .ALU_OUT(ALUOut));
+
+	// PhaseCounter
+	wire [4:0] phase;
+	PhaseCounter (.clock(clock), .phase(phase));
+
 	always @ (posedge clock) begin
-		// P1
-		P1Clock = phase == 5'b00001;
-		if (phase == 5'b00001) begin
-			begin
-				PCLoad <= GND;
-			end
-
-			// load memory
-			begin
-				memoryAddress <= PC;
-				memoryWriteEnable <= GND;
-			end
-		
-			// write IR
-			begin
-				IRWriteData <= memoryData;
-			end
-		end
-
 		// P2
 		if (phase == 5'b00010) begin
 			// calc, input, output
@@ -105,9 +113,8 @@ module Controller(
 				BR <= registerFile[IRData[13:11]];
 				AR <= registerFile[IRData[10:8]];
 			end
-
 			// load, store
-			if (IRData[15:14] == 2'b00 || IRData[15:14] == 2'b01) begin
+			else if (IRData[15:14] == 2'b00 || IRData[15:14] == 2'b01) begin
 				BR <= IRData[7:0];
 				AR <= registerFile[IRData[10:8]];
 			end
@@ -134,41 +141,20 @@ module Controller(
 			end
 
 			// load, store
-			if (IRData[15:14] == 2'b00 || IRData[15:14] == 2'b01) begin
+			else if (IRData[15:14] == 2'b00 || IRData[15:14] == 2'b01) begin
 				// P3
 				DR <= ALUOut;
 			end
 		
 			// load immidiate, branch
-			if (IRData[15:14] == 2'b10) ;
+			else if (IRData[15:14] == 2'b10) ;
 		end
 
 		// P4
 		if (phase == 5'b01000) begin
-			// load, store
-			if (IRData[15:14] == 2'b00 || IRData[15:14] == 2'b01) begin
-				if (IRData[15:14] == 2'b00) begin
-					// load
-					// P4
-					begin
-						memoryAddress <= DR;
-						memoryWriteEnable <= GND;
-					end
-					begin
-						MDR <= memoryData;
-					end
-					
-					// P5
-					begin
-						registerFile[IRData[13:11]] <= MDR;
-					end
-				end else begin
-					// store
-					// P4
-					memoryAddress <= DR;
-					memoryWriteData <= registerFile[AR];
-					memoryWriteEnable <= VCC;
-				end
+			// load
+			if (IRData[15:14] == 2'b00) begin
+				MDR <= memoryData;
 			end
 		end
 
@@ -193,16 +179,13 @@ module Controller(
 				end
 			end
 
-			// load, store
-			if (IRData[15:14] == 2'b00 || IRData[15:14] == 2'b01) begin
-				if (IRData[15:14] == 2'b00) begin
-					// P5
-					registerFile[IRData[13:11]] <= MDR;
-				end
+			// load
+			else if (IRData[15:14] == 2'b00) begin
+				registerFile[IRData[13:11]] <= MDR;
 			end
 		end
 	end
 
-	assign out = PC;
-	assign rf = registerFile[1];
+	assign out = registerFile[1];
+	assign rf = phase;
 endmodule
--- InstructionRegister.v
+++ InstructionRegister.v
@@ -1,12 +1,14 @@
 module InstructionRegister(
 	input         clock,
+	input         write,
 	input [0:15]  writeData,
 	output [0:15] loadData);
 
 	reg [15:0]    out = 16'b0;
 
-	always @ (posedge clock) begin
-		out = writeData;
+	always @ (write) begin
+		if (write)
+			out = writeData;
 	end
 
 	assign loadData = out;
